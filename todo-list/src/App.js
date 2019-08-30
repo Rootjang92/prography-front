fb812f92a376c3befba0a7a23694878496512626
장근호 <smith.jang@hutom.io>
2019-08-30 16:25:46 +0900

    commit message

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
-
-import TodoList from './components/TodoList';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Todo List</h1>
-      <TodoList />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
